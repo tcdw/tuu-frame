dd52cda9c8c6705c40ae225cda6048671f36e5f4
tcdw <tcdw2011@gmail.com>
2025-06-05 18:43:46 +0800

    refactor: Remove React from Electron renderer

---
+++ mv-player/src/main.ts
@@ -0,0 +1,4 @@
+import './app.ts';
+import './index.css'; // For global styles, if any
+
+console.log('Main script main.ts loaded.');
--- mv-player/vite.config.ts
+++ mv-player/vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import path from 'node:path'
 import electron from 'vite-plugin-electron/simple'
-import react from '@vitejs/plugin-react'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    
     electron({
       main: {
         // Shortcut of `build.lib.entry`.
